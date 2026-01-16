38c2c92a7e2783eb3adc98209f2fa49599f4ba88
yaza <gittnass@gmail.com>
2026-01-16 17:02:44 +0800

    masalah register dan login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,18 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <--- JANGAN LUPA INI
+use Illuminate\Support\Facades\URL; // <--- KUNCI UTAMA 1
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
     public function register(): void
     {
         //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        // Paksa HTTPS jika di Production (Railway)
+        // --- KUNCI UTAMA 2: PAKSA HTTPS DI RAILWAY ---
         if($this->app->environment('production')) {
             URL::forceScheme('https');
         }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         
-        // --- TAMBAHKAN BARIS INI ---
+        // --- TAMBAHKAN KODE INI ---
         $middleware->trustProxies(at: '*');
         
     })
