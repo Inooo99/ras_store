9ba5ec628416311fefb718ea5bfe1e0fbb3a5b8e
yaza <gittnass@gmail.com>
2026-01-16 16:17:25 +0800

    Fix login mental mental force HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- TAMBAHKAN INI (1)
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // --- TAMBAHKAN KODE INI (2) ---
+        // Jika sedang di Production (Railway), paksa gunakan HTTPS
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
