aedcfb8a1032805018963735198c3027cdfe11d7
yaza <gittnass@gmail.com>
2026-01-16 18:26:36 +0800

    Add migration route

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\CheckoutController;
 use App\Models\Product;
 use App\Models\Sosmed;
 use App\Models\Game;
+use Illuminate\Support\Facades\Artisan;
 
 // --- HALAMAN AUTH ---
 Route::get('/register', [UserController::class, 'showRegister']);
@@ -53,4 +54,18 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::resource('admin/products', ProductController::class)->names('products');
     Route::resource('admin/sosmed', SosmedController::class);
     Route::resource('admin/games', GameController::class);
+});
+
+// --- TOMBOL RAHASIA UNTUK MIGRASI ---
+Route::get('/paksa-migrasi', function () {
+    // 1. Bersihkan Cache
+    Artisan::call('optimize:clear');
+    
+    // 2. Paksa Migrasi Database
+    Artisan::call('migrate:fresh --force');
+    
+    // 3. Buat Storage Link
+    Artisan::call('storage:link');
+
+    return "<h1>SUKSES! Database sudah di-reset dan di-migrasi.</h1><br>" . nl2br(Artisan::output());
 });
